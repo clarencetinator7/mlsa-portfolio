f012467f98f639309c7456ff4b808b836c6707a7
Clarence Joseph Dimafelix <clarencejoseph.dimafelix.s@bulsu.edu.ph>
2023-12-06 12:55:29 +0000

    chore: edit information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Clarence Joseph Dimafelix",
+  title: "Front-end Developer",
+  email: "clarencejoseph.dimafelix@gmail.com",
+  gitHub: "clarencetinator7",
+  instagram: "clarence.the.dimafelix",
+  linkedIn: "clarencejoseph",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am Information Technology student from Bulacan State University. I thrive on learning new technologies and staying at the forefront of innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,15 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "UI/UX design",
+  "React",
+  "Node.js",
+  "MongoDB",
+  "Express",
+  "JavaScript",
+  "CSS",
+  "HTML",
 ];
 
 /**
@@ -45,7 +49,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "";
 
 const About = () => {
   return (
